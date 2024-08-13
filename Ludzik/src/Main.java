1d33b799cb183eea1b5c0ab602b517a2a5a3ae66
Dragonez51 <145779587+Dragonez51@users.noreply.github.com>
2024-08-13 10:48:16 +0200

    Add files via upload

--- Ludzik/src/Main.java
+++ Ludzik/src/Main.java
@@ -123,11 +123,12 @@ class LudzikGra{
 
     int failCount=0;
     boolean win;
+    boolean lose;
 
     LudzikGra(String haslo) throws IOException {
 //        gameMainPanel
         gameMainPanel.setLayout(new BoxLayout(gameMainPanel, BoxLayout.PAGE_AXIS));
-//        wordPanel.setLayout(new BoxLayout(wordPanel, BoxLayout.PAGE_AXIS));
+        wordPanel.setLayout(new BoxLayout(wordPanel, BoxLayout.PAGE_AXIS));
 //        gameMainFrame Part 1
         gameMainFrame.setTitle("Ludzik");
         gameMainFrame.add(gameMainPanel);
@@ -160,22 +161,34 @@ class LudzikGra{
         }
         JLabel letter = new JLabel();
         letter.setFont(new Font("Arial", Font.BOLD, 60));
+        letter.setAlignmentX(Component.CENTER_ALIGNMENT);
         LudzikGra.updateGuessedWord(letter, guess, failCount, haslo, win);
-        wordPanel.setBorder(new EmptyBorder(30,0,0,0));
+        wordPanel.setBorder(new EmptyBorder(40,0,0,0));
         wordPanel.setPreferredSize(new Dimension(1000,150));
         wordPanel.setMaximumSize(wordPanel.getPreferredSize());
         wordPanel.setBackground(new Color(100,100,100));
+        wordPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         wordPanel.add(letter);
         gameMainPanel.add(wordPanel);
-//        Panel for buttons
+//        Panels for buttons
+        //Main buttonPanel
         JPanel buttonPanel = new JPanel();
         buttonPanel.setBackground(new Color(100,100,100));
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.PAGE_AXIS));
+        //Top buttonPanel
+        JPanel buttonTopPanel = new JPanel();
+        buttonTopPanel.setBackground(new Color(100,100,100));
+        //Mid buttonPanel
+        JPanel buttonMidPanel = new JPanel();
+        buttonMidPanel.setBackground(new Color(100,100,100));
+        //Bottom buttonPanel
+        JPanel buttonBottomPanel = new JPanel();
+        buttonBottomPanel.setBackground(new Color(100,100,100));
 //        Buttons
-
-        for(int i=0; i<26; i++){
-            char ch = (char) (65+i);
+        char[] keyboard = {'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'};
+        for(int i=0; i<keyboard.length; i++){
             JButton key = new JButton();
-            key.setText(String.valueOf(ch));
+            key.setText(String.valueOf(keyboard[i]));
             key.setPreferredSize(new Dimension(80,80));
             key.setMaximumSize(key.getPreferredSize());
 //            CHECK FOR LETTERS IN WORD
@@ -196,17 +209,41 @@ class LudzikGra{
                     if(!hit){
                         failCount++;
                         key.setBackground(Color.DARK_GRAY);
+                        if(failCount == 10){
+                            lose = true;
+                        }
                     }
                     try {
                         updateImage(ludzik, picLabel, failCount);
                     } catch (IOException ex) {
                         ex.printStackTrace();
                     }
+
+                    for(int i=0; i<guess.length; i++){
+                        if("-".equals(guess[i])){
+                            win = false;
+                            break;
+                        }else{
+                            win = true;
+                        }
+                    }
+
+                    if(win){
+                        gameOver(buttonTopPanel, buttonMidPanel, buttonBottomPanel);
+                    }else if(lose){
+                        gameOver(buttonTopPanel, buttonMidPanel, buttonBottomPanel);
+                    }
+
                     updateGuessedWord(letter, guess, failCount, haslo, win);
                 }
             });
-            buttonPanel.add(key);
+            if(i<10){buttonTopPanel.add(key);}
+            else if(i>9 && i<19){buttonMidPanel.add(key);}
+            else{buttonBottomPanel.add(key);}
         }
+        buttonPanel.add(buttonTopPanel);
+        buttonPanel.add(buttonMidPanel);
+        buttonPanel.add(buttonBottomPanel);
         buttonPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         gameMainPanel.add(buttonPanel);
 //          gameMainFrame Part 2
@@ -216,14 +253,6 @@ class LudzikGra{
     public static void updateGuessedWord(JLabel letter, String[] guess, int failCount, String haslo, boolean win){
         if(failCount!=10) {
             letter.setText("");
-            for(int i=0; i<guess.length; i++){
-                if("-".equals(guess[i])){
-                    win = false;
-                    break;
-                }else{
-                    win = true;
-                }
-            }
             int i = 0;
             for (String temp : guess) {
                 letter.setText(letter.getText() + guess[i]);
@@ -234,6 +263,7 @@ class LudzikGra{
                 letter.setText("WYGRANA, HASŁO TO: "+haslo);
                 letter.setFont(new Font("Arial", Font.BOLD, 30));
                 letter.setForeground(Color.GREEN);
+
             }
 
         }else{
@@ -246,5 +276,19 @@ class LudzikGra{
     public static void updateImage(BufferedImage ludzik, JLabel picLabel, int  failCounter) throws IOException {
         ludzik = ImageIO.read(new File(System.getProperty("user.dir")+"/Image/Ludzik("+failCounter+").png"));
         picLabel.setIcon(new ImageIcon(ludzik));
+    }
+    public static void gameOver(JPanel buttonTopPanel, JPanel buttonMidPanel, JPanel buttonBottomPanel){
+        for(int i=0; i<buttonTopPanel.getComponentCount(); i++){
+            Component c = buttonTopPanel.getComponent(i);
+            c.setEnabled(false);
+        }
+        for(int i=0; i<buttonMidPanel.getComponentCount(); i++){
+            Component c = buttonMidPanel.getComponent(i);
+            c.setEnabled(false);
+        }
+        for(int i=0; i<buttonBottomPanel.getComponentCount(); i++){
+            Component c = buttonBottomPanel.getComponent(i);
+            c.setEnabled(false);
+        }
     }
 }
